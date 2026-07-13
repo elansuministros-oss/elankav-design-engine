7249ce86c0cb8dc84eb4e5703af9a15b48768a4f
elansuministros-oss <elansuministros@gmail.com>
2026-07-12 22:32:32 -0600

    DESIGN-003A configura motor real con OpenAI y QA

--- engine/createDefaultDesignEngine.js
+++ engine/createDefaultDesignEngine.js
@@ -2,15 +2,67 @@
 
 const { DesignEngine } = require('./DesignEngine');
 const {
-  DesignRequestService
+  DesignRequestService,
 } = require('../services/designRequestService');
+const {
+  PromptBuilderService,
+} = require('../services/promptBuilderService');
+const {
+  RenderService,
+} = require('../services/renderService');
+const {
+  VisualQaService,
+} = require('../services/visualQaService');
+const {
+  OpenAIImageAdapter,
+} = require('../adapters/image/OpenAIImageAdapter');
 
-function createDefaultDesignEngine() {
-  return new DesignEngine({
-    designRequestService: new DesignRequestService()
+function createDefaultDesignEngine(options = {}) {
+  const designRequestService =
+    options.designRequestService || new DesignRequestService();
+  const promptBuilderService =
+    options.promptBuilderService || new PromptBuilderService();
+  const imageAdapter =
+    options.imageAdapter || new OpenAIImageAdapter(options.openAI || {});
+  const renderService =
+    options.renderService || new RenderService({
+      imageAdapter,
+      promptBuilderService,
+      outputDir: options.outputDir,
+      publicBaseUrl: options.publicBaseUrl,
+    });
+  const visualQaService =
+    options.visualQaService || new VisualQaService();
+
+  const engine = new DesignEngine({
+    designRequestService,
+    renderService,
+    visualQaService,
   });
+
+  const executeCore = engine.execute.bind(engine);
+
+  engine.execute = async function execute(input) {
+    const result = await executeCore(input);
+    const approved = result.qa?.approved === true;
+    const hasSingleAsset = result.assets?.length === 1;
+    const processed = approved && hasSingleAsset;
+
+    return {
+      ...result,
+      designId: processed ? result.assets[0].id : null,
+      status: processed ? 'PROCESSED' : 'NEEDS_INFORMATION',
+      elanIaResult: {
+        ...result.elanIaResult,
+        conversational: false,
+        clientReady: processed,
+      },
+    };
+  };
+
+  return engine;
 }
 
 module.exports = {
-  createDefaultDesignEngine
+  createDefaultDesignEngine,
 };
